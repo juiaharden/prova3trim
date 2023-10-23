8127e2f5b7b33e67155634ff8f01e4ae573a8dc4
juiaharden <130366314+juiaharden@users.noreply.github.com>
2023-10-23 11:29:53 -0300

    Add files via upload

---
+++ brofazsol.js
@@ -0,0 +1,9 @@
+function Calcular(){
+
+    let idadeCanina = prompt("Qual a idade do cachorrinho?");
+    if (isNaN(idadeCanina)) {
+        alert("Por favor insira uma idade válida.");
+        return;
+    }
+
+}
